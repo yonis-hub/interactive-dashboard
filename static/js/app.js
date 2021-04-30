085e6d11d8bd89e751421c84efaded3850eeac5f
Yonis Hassan <hyonis@fb.com>
2021-04-29 15:49:39 -1000

    adding graph

--- static/js/app.js
+++ static/js/app.js
@@ -9,9 +9,38 @@ function drawBarGraph(sampleId) {
         // console.log(data);
 
         var samples = data.samples;
-        var resultArray = samples.filter(sample => sample.id == sampleId);
+        var resultArray = samples.filter(s=> s.id == sampleId);
         // console.log(resultArray);
-        var result = resultArray[0]; /// VIDEO 59:01 left off
+        var result = resultArray[0]; 
+        // console.log(result);
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        // console.log(otu_labels);
+
+        var sample_values = result.sample_values;
+        // console.log(sample_values);
+
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
+        }
+
+        //plot to plotly
+        var barArray = [barData];
+
+        var barLayout = {
+            title: 'Top 10 Bacteria Cultures Found',
+            margin: {t:30, l:150}
+        }
+
+        Plotly.newPlot('bar',barArray, barLayout)
+
     });
 
 }
